e0368d66c610740bb9d94c56b937a209ddc59d34
Rohtas Wadera <rohtaswadera@gmail.com>
2018-03-30 10:42:47 -0400

    catch me if you can game complete with plenty of jQuery

--- catch.js
+++ catch.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 	var canvas = $("#myCanvas").get(0);
 	var context = canvas.getContext("2d");
-	var bigX = 50, bigY = 100, bigLength = 50;
-	var smallX = 0, smallY = 0, smallLength = 25;
-	var up = false, down = false, left = false, right = false;
-	var frameDistance = 5;
+	var bigX, bigY, bigLength;
+	var smallX, smallY, smallLength;
+	var up, down, left, right;
+	var frameDistance; 
+	var score; 
+	var timer; 
+	var id;
 	context.fillStyle = "maroon";
 	context.fillRect(bigX, bigY, bigLength, bigLength);
 	context.fillStyle = "lightgray";
 	context.fillRect(smallX, smallY, smallLength, smallLength);
+
+
+function firstValues() {
+	bigX = 50, bigY = 100, bigLength = 50;
+	smallX = 0, smallY = 0, smallLength = 25;
+	up = false, down = false, left = false, right = false;
+	frameDistance = 5, score = 0, timer = 30, id = null;
+}
+
 
 function moveTarget() {
 	smallX = Math.floor(Math.random() * (canvas.width - smallLength));
@@ -54,12 +66,25 @@ function redraw() {
 		bigX = canvas.width - bigLength;
 	if(bigY + bigLength > canvas.height)
 		bigY = canvas.height - bigLength;
-
+	if(touch(smallX, bigX, bigX +  bigLength) || touch(smallX, smallLength, bigX, bigX + bigLength)) {
+		if(touch(smallY, bigY, bigY +bigLength) || touch(smallY+smallLength, bigY, bigY + bigLength)) {
+			moveTarget();
+			score++;
+		}
+	}
 context.fillStyle = "maroon";
 context.fillRect(bigX, bigY, bigLength, bigLength);
 context.fillStyle = "lightgray";
 context.fillRect(smallX, smallY, smallLength, smallLength);
+$("#result").text("Score: " + score + " " + "Time left: " + timer);
+if(timer <= 0) {
+	clearInterval(id);
+	clearBackground();
+	$("#result").text("Final Score: " + score);
+}
+else {
 window.requestAnimationFrame(redraw);
+}
 
 }
 
@@ -68,14 +93,23 @@ function clearBackground() {
 	context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function touch(inside, first, second) {
+		return(inside > first && inside < second)
+}
+
 function startGame() {
+	id = setInterval(function () {
+		timer--;
+	}, 1000);
 	moveTarget();
 	redraw();
 	canvas.focus();
 }
-
-startGame();
-
+$("#play").click(function () {
+	alert("Use Arrow Keys to Play!");
+	firstValues();
+	startGame();
+})
 
 
 });
